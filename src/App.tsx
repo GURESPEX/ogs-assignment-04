b7319e6d2abf8f0c0b4fd87a9ffa5ef5f43f448c
lonelypuppy <118647548+GURESPEX@users.noreply.github.com>
2024-05-07 09:15:04 +0700

    [Delete]: Delete page transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import Router from "@/router/Router";
-import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   return (
     <>
       <div className="col items-center w-screen h-screen overflow-auto bg-white">
         <div className="col w-full h-full max-w-[1280px]">
-          <AnimatePresence mode="wait">
-            <Router />
-          </AnimatePresence>
+          <Router />
         </div>
       </div>
     </>
